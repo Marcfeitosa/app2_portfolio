3daf1af70fee76e384b9b7c32aa057a01a2d9592
Márcio Feitosa <marcfeitosa@yahoo.com.br>
2024-01-05 12:54:28 -0300

    Day 22: Render the app titles in two columns

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
@@ -8,7 +9,7 @@ with col1:
     st.image("images/photo.png")
 
 with col2:
-    st.title("ardit Sulce")
+    st.title("Márcio Feitosa")
     content = """
     plug in my test"""
 
@@ -19,3 +20,14 @@ Below you can find some of the app I have built in Python. Feel free to contact 
 """
 
 st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pd.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row['title'])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row['title'])
