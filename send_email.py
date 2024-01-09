f74b311702b97b1af2143c75cd0c4195ec87d30f
Márcio Feitosa <marcfeitosa@yahoo.com.br>
2024-01-09 10:21:12 -0300

    Day 22: testing e-mail sending

--- send_email.py
+++ send_email.py
@@ -1,22 +1,17 @@
-import smtplib
-import ssl
+import smtplib, ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-password = "oknh jqss icgm kxje"
-username = "marcfeitosa1@gmail.com"
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
 
-receiver = "marcfeitosa1@gmail.com"
-context = ssl.create_default_context()
+    password = "oknh jqss icgm kxje"
+    username = "marcfeitosa1@gmail.com"
 
-message = """\
-Subject: Greetings from the app
-Hi, how are you?
-Bye!
-"""
+    receiver = "marcfeitosa1@gmail.com"
+    context = ssl.create_default_context()
 
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
 
